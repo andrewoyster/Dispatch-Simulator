227c270884255001d399ba74bdab5b08fbf69485
andrew.oyster <aoyster26@gmail.com>
2026-09-05 15:20:32 -0400

    Add alert screen animation

--- script.js
+++ script.js
@@ -6,9 +6,35 @@ for (let i = 0; i < numberOfDispatchTypes; i++) {
   document
     .querySelectorAll(".dispatch")
     [i].addEventListener("click", function () {
-      console.log("I just got clicked!");
-      console.log(this);
       var dispatchType = this.innerHTML;
-      console.log(dispatchType);
+      dispatchCall(dispatchType);
     });
+}
+
+function dispatchCall(dispatchType) {
+  let dispatchMessage = document.querySelector(".dispatch-message");
+
+  switch (dispatchType) {
+    case "Medical":
+      dispatchMessage.innerHTML = "Medical Call Dispatched";
+      break;
+    case "Fire":
+      dispatchMessage.innerHTML = "Fire Assignment Dispatched";
+      break;
+    case "MVC":
+      dispatchMessage.innerHTML = "MVC Assignment Dispatched";
+      break;
+    case "Cardiac Arrest":
+      dispatchMessage.innerHTML = "Cardiac Arrest Assignment Dispatched";
+      break;
+    default:
+      console.log("Unknown dispatch type: " + dispatchType);
+  }
+
+  let alertScreen = document.querySelector(".screen");
+  alertScreen.classList.add("screen-alert");
+
+  setTimeout(function () {
+    alertScreen.classList.remove("screen-alert");
+  }, 300);
 }
